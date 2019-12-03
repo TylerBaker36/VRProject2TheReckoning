4bb3ed508185f382821c74d98c53089566d98e33
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-12-03 16:05:04 -0500

    Update the testing UI

---
+++ VRProject2TheReckoning/Assets/Scenes/Temporary/Scripts/Editor/InspectorEditor.cs
@@ -0,0 +1,21 @@
+﻿// InspectorEditor (Completed) - Shijun Guo
+using UnityEngine;
+using UnityEditor;
+
+#region Other Editor
+[CustomEditor(typeof(Renamer))]
+public class RenamerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Renamer renamer = (Renamer)target;
+
+        if (GUILayout.Button("Rename Children"))
+        {
+            renamer.RenameChildren();
+        }
+    }
+}
+#endregion
