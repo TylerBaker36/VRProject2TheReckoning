b59c706ba1c39805977d3376493d963d5030a54c
Stephen Brasel <wearyroad@gmail.com>
2019-12-05 14:45:42 -0500

    Fixes to folder hierarchy organization.
    Oculus folder is in the Assets folder directly. DO NOT MOVE.

---
+++ VRProject2TheReckoning/Assets/Scenes/Temporary/Scripts/Renamer.cs
@@ -0,0 +1,20 @@
+﻿// ShijunGuo (2019-06-07)
+// Rename Children GameObject under a Parent.
+using UnityEngine;
+
+public class Renamer : MonoBehaviour
+{
+    [SerializeField] private string mainTitle;
+
+    public void RenameChildren()
+    {
+        int counter = 0;
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.name = mainTitle + counter.ToString();
+
+            counter++;
+        }
+    }
+}
