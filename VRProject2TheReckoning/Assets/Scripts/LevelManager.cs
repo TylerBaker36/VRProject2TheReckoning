de0307f5469092238f76cdde8271863fdf311b72
Jonathan Burnett <jburnett96@gmail.com>
2019-12-05 13:26:36 -0500

    Add CornHole and Hub scenes

---
+++ VRProject2TheReckoning/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private string scene_name;
+
+    private void OnTriggerEnter(Collider ChangeScene)
+    {
+        if(ChangeScene.gameObject.CompareTag("Player"))
+        {
+            //Application.LoadLevelAdditive(1);
+            SceneManager.LoadScene(scene_name);
+        }
+    }
+}
