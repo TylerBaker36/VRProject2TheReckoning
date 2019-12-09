eb6b9c4acf106a98bc9cf4354b02568179705bd9
Jonathan Burnett <jburnett96@gmail.com>
2019-12-09 15:55:43 -0500

    Add scripts for Cornhole functionality

--- VRProject2TheReckoning/Assets/Scripts/CornHole.cs
+++ VRProject2TheReckoning/Assets/Scripts/CornHole.cs
@@ -7,24 +7,42 @@ public class CornHole : MonoBehaviour
     private int beanbag_count = 4;
     private GameObject[] beanbags;
 
+    [SerializeField] private GameObject scoreKeeper;
+
+    // Handles reseting the beanbags on round end
     private void OnTriggerExit(Collider CheckBags)
     {
-        if(CheckBags.gameObject.CompareTag("Beanbag"))
+        if (CheckBags.gameObject.name == "StagingArea")
         {
-            beanbag_count--;
-        }
-
-        if(beanbag_count == 0)
-        {
-            StartCoroutine(wait());
-            // reset positions
-            if (beanbags == null)
-                beanbags = GameObject.FindGameObjectsWithTag("Beanbag");
-            foreach (GameObject b in beanbags)
+            if (CheckBags.gameObject.CompareTag("Beanbag"))
             {
-                b.GetComponent<Beanbag>().Respawn();
+                beanbag_count--;
             }
-            beanbag_count = 4;
+
+            if (beanbag_count == 0)
+            {
+                StartCoroutine(wait());
+                // reset positions
+                if (beanbags == null)
+                    beanbags = GameObject.FindGameObjectsWithTag("Beanbag");
+                foreach (GameObject b in beanbags)
+                {
+                    b.GetComponent<Beanbag>().Respawn();
+                }
+                beanbag_count = 4;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider GoalTriggers)
+    {
+        if (GoalTriggers.gameObject.name == "Goal Trigger")
+        {
+
+        }
+        else if (GoalTriggers.gameObject.name == "Board Trigger")
+        {
+
         }
     }
 
---
+++ VRProject2TheReckoning/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    private int rscore, oscore; // round and overall scores
+    [SerializeField] private Text roundScore;
+    [SerializeField] private Text overallScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rscore = 0;
+        oscore = 0;
+        InitializeTextField(ref roundScore, "00");
+        InitializeTextField(ref overallScore, "00");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Score(int val)
+    {
+        rscore+=val;
+        oscore+=val;
+        roundScore.text = string.Format("{0:00}", rscore.ToString().PadLeft(2, '0'));
+        overallScore.text = string.Format("{0:00}", oscore.ToString().PadLeft(2, '0'));
+    }
+
+    public bool RoundReset()
+    {
+        rscore = 0;
+        roundScore.text = "00";
+        return (roundScore.text == "00");
+    }
+
+    public bool ScoreReset()
+    {
+        oscore = 0;
+        overallScore.text = "00";
+        return (overallScore.text == "00");
+    }
+
+    private void InitializeTextField(ref Text component, string textName)
+    {
+        if (component == null)
+        {
+            Text[] Texts = Resources.FindObjectsOfTypeAll<Text>();
+            foreach (Text item in Texts)
+            {
+                if (item.name == textName)
+                {
+                    component = item;
+                }
+            }
+        }
+    }
+}
