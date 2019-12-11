621782336d5c2e915a915338484db19ba9f85cc1
Stephen Brasel <wearyroad@gmail.com>
2019-12-11 15:13:28 -0500

    PlayerW/CollidingHands - if you put that in your scene, you can punch things.
    MeshManager changes the shape of meshes to reflect their in-game state (as opposed to wielding an open-hand mesh when in fact your hand is giving the thumbs up.)

---
+++ VRProject2TheReckoning/Assets/Scripts/MeshManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshManager : MonoBehaviour
+{
+    [SerializeField] 
+    [Tooltip("(delta)Time in seconds before the mesh is rebaked.")] 
+    private float refreshInterval = 0.5f;
+
+    private SkinnedMeshRenderer meshRenderer;
+    private MeshCollider collider;
+    private Mesh mesh;
+    private float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<SkinnedMeshRenderer>();
+        collider = GetComponent<MeshCollider>();
+        mesh = meshRenderer.sharedMesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= refreshInterval)
+        {
+            time = 0;
+            UpdateCollider();
+        }
+    }
+
+    public void UpdateCollider()
+    {
+        Mesh colliderMesh = new Mesh();
+        meshRenderer.BakeMesh(colliderMesh);
+        collider.sharedMesh = null;
+        collider.sharedMesh = colliderMesh;
+    }
+}
