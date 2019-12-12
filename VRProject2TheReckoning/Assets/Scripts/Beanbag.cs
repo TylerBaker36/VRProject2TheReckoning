7fafcbc91907d3ceca5747c4d2e19436f29d2813
Jonathan Burnett <jburnett96@gmail.com>
2019-12-12 17:50:50 -0500

    Temporary fix for early reset bug (more permanent fix WIP)

--- VRProject2TheReckoning/Assets/Scripts/Beanbag.cs
+++ VRProject2TheReckoning/Assets/Scripts/Beanbag.cs
@@ -5,17 +5,42 @@ using UnityEngine;
 public class Beanbag : MonoBehaviour
 {
     private Vector3 start_pos;
+    public bool picked_up; // Set to true first time the beanbag is picked up
+    public bool thrown; // Set to true first time the beanbag is thrown
 
     // Start is called before the first frame update
     void Start()
     {
-         start_pos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        start_pos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        picked_up = false;
+        thrown = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    public bool getPickedUp() { return picked_up; }
+    public bool getThrown() { return thrown; }
+
+    public bool updatePickedUp()
+    {
+        if (this.GetComponent<OVRGrabbable>().isGrabbed)
+            picked_up = true;
+        else
+            picked_up = false;
+        return picked_up;
+    }
+
+    public bool updateThrown()
+    {
+        if (this.GetComponent<OVRGrabbable>().isGrabbed)
+            thrown = false;
+        else
+            thrown = false;
+        return thrown;
     }
 
     public void Respawn()
@@ -24,5 +49,6 @@ public class Beanbag : MonoBehaviour
         gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
         gameObject.transform.position = start_pos;
+        picked_up = false;
     }
 }
--- VRProject2TheReckoning/Assets/Scripts/CornHole.cs
+++ VRProject2TheReckoning/Assets/Scripts/CornHole.cs
@@ -4,12 +4,42 @@ using UnityEngine;
 
 public class CornHole : MonoBehaviour
 {
-    private int beanbag_count = 4;
+    private int beanbag_count = 8;
     private GameObject[] beanbags;
+    private GameObject last_bag_held;
+    //private bool all_thrown;
 
     [SerializeField] private GameObject scoreKeeper;
 
-    // Handles reseting the beanbags on round end
+    /*private void Start()
+    {
+        all_thrown = false;
+    }*/
+
+    private void Update()
+    {
+        /*if (last_bag_held != null)
+        {
+            if (last_bag_held.GetComponent<Beanbag>().getPickedUp())
+                last_bag_held.GetComponent<Beanbag>().updateThrown(); // if the bag has been held, now check if it's been thrown
+        }*/
+        if (beanbag_count == 0)
+        {
+            /*all_thrown = true;
+            foreach (GameObject b in beanbags)
+            {
+                if ((!b.GetComponent<Beanbag>().getThrown()) || (!b.GetComponent<Beanbag>().getPickedUp()))
+                {
+                    all_thrown = false; // if we find one that hasn't been thrown or picked up, all_thrown set to false
+                }
+            }
+            if (all_thrown)*/
+            if (!last_bag_held.GetComponent<OVRGrabbable>().isGrabbed)
+                StartCoroutine(wait_then_reset(5.0f)); // reset positions in coroutine
+        }
+    }
+
+    // Each time something exits the stagingarea, check if it's a beanbag
     private void OnTriggerExit(Collider CheckBags)
     {
         if (this.name == "StagingArea")
@@ -18,28 +48,22 @@ public class CornHole : MonoBehaviour
             if (CheckBags.gameObject.CompareTag("Beanbag"))
             {
                 beanbag_count--;
-            }
-
-            if (beanbag_count == 0)
-            {
-                StartCoroutine(wait_then_reset(5.0f)); // reset positions in coroutine
+                last_bag_held = CheckBags.gameObject;
+                //last_bag_held.GetComponent<Beanbag>().updatePickedUp();
             }
         }
     }
 
     private void OnTriggerEnter(Collider GoalTriggers)
     {
-        if (this.name == "GoalTrigger")
+        if (GoalTriggers.gameObject.CompareTag("Beanbag"))
         {
-            if (GoalTriggers.gameObject.CompareTag("Beanbag"))
+            if (this.name == "GoalTrigger")
             {
                 // We scored! Only give 2 points here (out of the full 3) because passing through the board trigger will always yield a point
                 scoreKeeper.GetComponent<ScoreKeeper>().Score(2);
             }
-        }
-        else if (this.name == "BoardTrigger")
-        {
-            if (GoalTriggers.gameObject.CompareTag("Beanbag"))
+            else if (this.name == "BoardTrigger")
             {
                 // Since it is impossible to score in the goal without passing through the board trigger, always award 1 point even to those who just pass through (for now)
                 scoreKeeper.GetComponent<ScoreKeeper>().Score(1);
@@ -52,11 +76,16 @@ public class CornHole : MonoBehaviour
         yield return new WaitForSeconds(f);
         if (beanbags == null)
             beanbags = GameObject.FindGameObjectsWithTag("Beanbag");
+        //while (last_bag_held.gameObject.GetComponent<Beanbag>().updatePickedUp() != false)
+        //{
+            // block until the last bag has left player's hands
+        //}
         foreach (GameObject b in beanbags)
         {
             b.GetComponent<Beanbag>().Respawn();
         }
         beanbag_count = 4;
+        //all_thrown = false;
         scoreKeeper.GetComponent<ScoreKeeper>().RoundReset(); // Reset round but not overall score
     }
 }
