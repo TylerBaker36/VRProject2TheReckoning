6d6ffaeeccf9bf99902b5f0ea9e6c2811dad2b41
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-12-12 16:13:42 -0500

    Golf Respowner and Destroyer

---
+++ VRProject2TheReckoning/Assets/Scripts/GolfController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject gameObject = collision.gameObject;
+
+        if (gameObject.tag == "Target")
+        {
+            GolfRespawner golfRespawner = gameObject.transform.parent.GetComponent<GolfRespawner>();
+            this.transform.position = golfRespawner.Respawner.transform.position;
+        }
+    }
+}
