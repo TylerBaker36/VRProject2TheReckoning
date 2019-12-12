03d0b7a2ec9c77f25af2e3e192d54ad9bad95e3d
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-12-12 10:09:42 -0500

    Add destroy methods to the bullet and shell

---
+++ VRProject2TheReckoning/Assets/Scripts/DestoryShell.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryShell : MonoBehaviour
+{
+    float timer = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.time;
+
+        if (timer < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
