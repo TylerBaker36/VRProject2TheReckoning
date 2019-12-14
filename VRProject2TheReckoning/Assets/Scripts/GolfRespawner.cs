7718fab3e1ad2de0e587843bcaf17e08e7c74dbf
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-12-13 19:44:04 -0500

    Add a Create Golf Ball methods to mini golf

--- VRProject2TheReckoning/Assets/Scripts/GolfRespawner.cs
+++ VRProject2TheReckoning/Assets/Scripts/GolfRespawner.cs
@@ -6,7 +6,18 @@ public class GolfRespawner : MonoBehaviour
 {
     public Transform target;
     public Transform respawner;
+    public GameObject golfBall;
 
+    private void Start()
+    {
+        CreateGolfBall();
+    }
+
+    private void CreateGolfBall()
+    {
+        Instantiate(golfBall);
+        golfBall.transform.position = respawner.position;
+    }
     public void TeleportBall(GameObject gameObject)
     {
         Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
