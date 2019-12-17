edb98f7c3af0ea412d794c30ad76078ce3f14aa0
YaBoiRayden <Tradderley@gmail.com>
2019-12-17 17:05:56 -0500

    object lerp functionality

--- VRProject2TheReckoning/Assets/Scripts/ObjectMoveTo.cs
+++ VRProject2TheReckoning/Assets/Scripts/ObjectMoveTo.cs
@@ -4,32 +4,75 @@ using UnityEngine;
 
 public class ObjectMoveTo : MonoBehaviour
 {
+    [SerializeField] Transform nextLocation;
     [SerializeField] Transform PointA;
     [SerializeField] Transform PointB;
     [SerializeField] bool alwaysOn;
-    [SerializeField] float moveTime = 5;
+    [SerializeField] bool completed;
+
+    float startTime = 0;
+    [SerializeField] float speed;
+    [SerializeField] float deltaTime = 0;
+    [SerializeField] float lerpAmount;
+
+    [SerializeField] float delay = 1;
+    float waitTimer = 1;
+
+    float pointDistance;
+    [SerializeField] float objectDistance;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        deltaTime = startTime;
+        nextLocation = PointB;
+        waitTimer = delay;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
+        pointDistance = Vector3.Distance(PointA.position, PointB.position);
+        objectDistance = Vector3.Distance(transform.position, nextLocation.position);
+
+        deltaTime += Time.deltaTime;
+
+        lerpAmount = (speed * deltaTime) / pointDistance;
+
+        if(objectDistance <= .05f)
+        {
+            transform.position = nextLocation.position;
+        }
+        if (transform.position == nextLocation.position)
+        {
+            completed = true;
+        }
+
+        if (completed)
+        {
+            waitTimer -= Time.deltaTime;
+            deltaTime = 0;
+        }
+        if (transform.position == PointB.position  && waitTimer <= startTime)
+        {
+            completed = false;
+            nextLocation = PointA;
+            waitTimer = delay;
+        }
+
+        if (alwaysOn && transform.position == PointA.position && waitTimer <= startTime)
+        {
+            completed = false;
+            nextLocation = PointB;
+            waitTimer = delay;
+        }
+        MoveToLocation();
     }
 
-    private void FixedUpdate()
+    void MoveToLocation()
     {
-        if (transform.position == PointA.position)
-        {
-            transform.position = Vector3.MoveTowards(PointA.position, PointB.position, moveTime * Time.deltaTime);
-        }
-        else if (transform.position == PointB.position)
-        {
-            transform.position = Vector3.MoveTowards(PointB.position, PointA.position, moveTime * Time.deltaTime);
-        }
+        transform.position = Vector3.Lerp(transform.position, nextLocation.position, lerpAmount);
     }
 }
