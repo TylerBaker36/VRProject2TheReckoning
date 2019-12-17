1f6ccb0b049a303bd6597ec8fe28204eafaae854
YaBoiRayden <Tradderley@gmail.com>
2019-12-17 17:24:47 -0500

    teleport functionality

---
+++ VRProject2TheReckoning/Assets/Scripts/ObjectTeleport.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTeleport : MonoBehaviour
+{
+    Ball ballObject;
+    Rigidbody objectRB;
+    [SerializeField] GameObject start;
+    [SerializeField] GameObject destination;
+    bool inLocation = false;
+    [SerializeField] bool teleport = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballObject = FindObjectOfType<Ball>();
+        objectRB = ballObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        start = other.gameObject;
+        if (other.gameObject == ballObject.gameObject)
+        {
+            ballObject.gameObject.transform.position = destination.transform.position;
+        }
+    }
+}
