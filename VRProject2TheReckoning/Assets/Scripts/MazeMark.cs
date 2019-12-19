104e31e86299ce59759caa35154ebf8be89030c2
YeeLFullSail <58181958+YeeLFullSail@users.noreply.github.com>
2019-12-19 16:33:59 -0500

    Corn Maze change
    Smaller maze. Players can throw the mark to the way with the controller. Also add the doors and keys in it.

---
+++ VRProject2TheReckoning/Assets/Scripts/MazeMark.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeMark : MonoBehaviour
+{
+    public GameObject mark;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One) || Input.GetKeyDown("z"))
+        {
+            Vector3 spawnPosition = transform.position;
+            Vector3 direction = Quaternion.Euler(0, 180, 10) * Vector3.right;
+            spawnPosition = transform.position + direction * 2;
+            Instantiate(mark, spawnPosition, Quaternion.identity);
+            }
+
+    }
+}
+
+
