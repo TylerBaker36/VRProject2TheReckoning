bbf50c322f3c952a88398b9a44cd61cc2d0818b1
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-12-19 09:44:41 -0500

    Add a ball generation

---
+++ VRProject2TheReckoning/Assets/Scripts/GenerateGolfBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateGolfBall : MonoBehaviour
+{
+    public GameObject golfBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            Instantiate(golfBall).transform.position = gameObject.transform.position;
+        }
+    }
+}
