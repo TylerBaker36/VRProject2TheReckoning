0b2d46329e0d7d4d04895c6b06f3956ed017ba18
Stephen Brasel <wearyroad@gmail.com>
2019-12-19 11:44:06 -0500

    Lock and Key script with prefab example.
    Locks will only unlock if the key has the exact name given.

---
+++ VRProject2TheReckoning/Assets/Scripts/ToggleLock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class BoolEvent : UnityEvent<bool>
+{
+
+}
+
+/// <summary>
+/// If this script is put on a GameObject with a trigger collider, it can be locked/unlocked by another GameObject with the tag "Key" as long as the GameObject has the same name as "specificKey".
+/// </summary>
+public class ToggleLock : MonoBehaviour
+{
+    [SerializeField] private bool lockstate = true;
+    [SerializeField] private string specificKey;
+    public BoolEvent lockToggled;
+
+    private void Awake()
+    {
+        if (lockToggled == null) 
+            lockToggled = new BoolEvent();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Key") && other.gameObject.name == specificKey)
+        {
+            lockstate = !lockstate;
+            lockToggled.Invoke(lockstate);
+        }
+    }
+}
